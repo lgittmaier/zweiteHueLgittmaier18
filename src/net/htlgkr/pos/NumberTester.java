4c6dbe4f1e60544029c3299a857ed5042157e0a6
Lukas GITTMAIER <lgittmaier18@htl-grieskirchen.local>
2020-09-28 15:00:54 +0200

    second commit

--- src/net/htlgkr/pos/NumberTester.java
+++ src/net/htlgkr/pos/NumberTester.java
@@ -5,14 +5,58 @@
  */
 package net.htlgkr.pos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  *
  * @author lgittmaier18
  */
 public class NumberTester {
-    
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
     private String fileName;
     public NumberTester(String fileName){
         this.fileName = fileName;
+    }
+    
+    
+    
+    public void setOddEvenTester(NumberTest oddTester){
+        this.oddTester = oddTester;
+    }
+    
+    public void setPrimeTester(NumberTest primeTester){
+        this.primeTester = primeTester;
+    } 
+    
+    public void setPalindromeTEster(NumberTest palindromeTester){
+        this.palindromeTester = palindromeTester;
+    }
+    
+    public void testFile() throws FileNotFoundException{
+        File file = new File("data");
+        
+        if(!file.canRead() || !file.isFile()){
+            System.exit(0);
+        }
+        
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(datName));
+            String zeile = null;
+            while ((zeile = in.readLine()) != null) {
+                System.out.println("Gelesene Zeile: " + zeile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); 
+    
+}
+    
+    public interface NumberTest{
+        boolean testNumber(int number);
     }
 }
